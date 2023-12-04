ea94f0cebb0372a1da46e9a04bc6667e677800b8
ignaciocuello <icuelloreyes97@gmail.com>
2023-12-04 12:48:45 +1100

    Implement fetch sutta as markdown

--- src/fetch_sutta_as_markdown.js
+++ src/fetch_sutta_as_markdown.js
@@ -1,6 +1,36 @@
+const get_sutta_json = require('./get_sutta_json');
+
 // fetch sutta using suttacentral get api
-function fetch_sutta_as_markdown(sutta) {
-    sutta_json = get_sutta_json(sutta);
+async function fetch_sutta_as_markdown(sutta) {
+    let sutta_json = await get_sutta_json(sutta);
+    //TODO: get title from suttaplex card
+    const title = sutta_json.translation_text[sutta_json.keys_order[2]];
+    let output = '';
+    output += `#### ${title}\n\n`
+    output += translation_body(sutta_json);
+    return output;
+}
+
+function translation_body(sutta_json) {
+    let body = '';
+    const keys = sutta_json.keys_order.slice(3);
+    for (let key of keys) {
+        let text = sutta_json.translation_text[key]
+        if (text) {
+            if (sutta_json.html_text[key].startsWith('<p>')) {
+                body += '>'
+            }
+            if (sutta_json.html_text[key].endsWith('</p>')) {
+                body += `${text.trim()}\n>\n`;
+            } else {
+                body += text;
+            }
+        }
+    }
+    if (body.endsWith('\n>\n')) {
+        body = body.slice(0, -3);
+    }
+    return body;
 }
 
 module.exports = fetch_sutta_as_markdown;
--- test/fetch_sutta_as_markdown.test.js
+++ test/fetch_sutta_as_markdown.test.js
@@ -1,8 +1,9 @@
 const fetch_sutta_as_markdown = require('../src/fetch_sutta_as_markdown');
 const fs = require('fs');
 
-test('Return content for SN 12.48 formatted as markdown', () => {
-    const formatted_sutta_text = fs.readFileSync('test/data/SN12.48.md', 'utf8');
-    const content = fetch_sutta_as_markdown('SN 12.48');
+test('Return content for SN 12.48 formatted as markdown', async () => {
+    let formatted_sutta_text = fs.readFileSync('test/data/SN12.48.md', 'utf8');
+    formatted_sutta_text = formatted_sutta_text.split('\n').slice(2).join('\n');
+    const content = await fetch_sutta_as_markdown('SN 12.48');
     expect(content).toBe(formatted_sutta_text);
 });
