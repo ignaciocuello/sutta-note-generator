d5b6df32ecd6f88f7a7a3de2192364634ffae77e
ignaciocuello <icuelloreyes97@gmail.com>
2023-12-04 14:13:51 +1100

    Implement sutta note content

--- src/sutta_note_content.js
+++ src/sutta_note_content.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+const fetch_sutta_as_markdown = require('./fetch_sutta_as_markdown');
+
 // If the output file is empty then just return the sutta text, otherwise return 
 // the updated sutta text in the existing file, but keep everything else intact.
-function sutta_note_content(sutta, output_directory) {
+async function sutta_note_content(sutta, output_directory) {
     const sutta_file = path.join(output_directory, `${sutta}.md`);
     const existing_content = fs.existsSync(sutta_file) ? fs.readFileSync(sutta_file, 'utf8') : '';
     // TODO: write fetch_sutta_as_markdown
-    const sutta_text = fetch_sutta_as_markdown(sutta)
+    const sutta_text = await fetch_sutta_as_markdown(sutta)
     if (existing_content) {
         // TODO: merge
+    } else {
+        return `### Sutta\n\n${sutta_text}`
     }
     return sutta_text;
 }
