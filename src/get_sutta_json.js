32db89ca4800f303240d0048f9b58c961f6c2491
ignaciocuello <icuelloreyes97@gmail.com>
2023-12-06 21:25:02 +1100

    Fetch ranged suttas

--- src/get_sutta_json.js
+++ src/get_sutta_json.js
@@ -1,6 +1,8 @@
 async function get_sutta_json(sutta) {
-    const sutta_id = get_sutta_id(sutta);
-    const url = `https://suttacentral.net/api/bilarasuttas/${sutta_id}/sujato?lang=en`; 
+    const suttaplex_json = await get_suttaplex_json(sutta);
+
+    const uid = suttaplex_json.suttaplex.uid
+    const url = `https://suttacentral.net/api/bilarasuttas/${uid}/sujato?lang=en`; 
 
     const response = await fetch(url);
     if (!response.ok) {
--- test/get_sutta_json.test.js
+++ test/get_sutta_json.test.js
@@ -7,8 +7,20 @@ beforeEach(() => {
 })
 
 test('Return json for SN 12.48', async () => {
-    const mock_json = fs.readFileSync('test/data/SN 12.48.json', 'utf8');
-    fetchMock.mockResponseOnce(mock_json);
+    const mock_sutta_json = fs.readFileSync('test/data/SN 12.48.json', 'utf8');
+    const mock_suttaplex_json = fs.readFileSync('test/data/SN 12.48.suttaplex.json', 'utf8');
+    fetchMock.mockIf(/^https:\/\/suttacentral\.net.*$/, async req => {
+        if (req.url.endsWith('/api/bilarasuttas/sn12.48/sujato?lang=en')) {
+            return mock_sutta_json;
+        } else if (req.url.endsWith('/api/suttas/sn12.48/sujato?lang=en&siteLanguage=en')) {
+            return mock_suttaplex_json;
+        } else {
+            return {
+                status: 404,
+                body: 'Not Found'
+            }
+        }
+    });
     const sutta_json = await get_sutta_json('SN 12.48');
     expect(sutta_json.root_text['sn12.48:0.3']).toBe('Lokāyatikasutta ');
     expect(sutta_json.translation_text['sn12.48:0.3']).toBe('A Cosmologist ');
@@ -22,4 +34,22 @@ test('Return json for suttaplex SN 12.48', async () => {
     expect(suttaplex_json.translation.title).toBe('A Cosmologist ');
 });
 
-//TODO: test for range of suttas e.g. AN /dhp
+test('Return json for AN 1.1', async () => {
+    const mock_sutta_json = fs.readFileSync('test/data/AN 1.1-10.json', 'utf8');
+    const mock_suttaplex_json = fs.readFileSync('test/data/AN 1.1.suttaplex.json', 'utf8');
+    fetchMock.mockIf(/^https:\/\/suttacentral\.net.*$/, async req => {
+        if (req.url.endsWith('/api/bilarasuttas/an1.1-10/sujato?lang=en')) {
+            return mock_sutta_json;
+        } else if (req.url.endsWith('/api/suttas/an1.1/sujato?lang=en&siteLanguage=en')) {
+            return mock_suttaplex_json;
+        } else {
+            return {
+                status: 404,
+                body: 'Not Found'
+            }
+        }
+    });
+    const sutta_json = await get_sutta_json('AN 1.1');
+    expect(sutta_json.root_text['an1.1:1.1']).toBe('Evaṁ me sutaṁ—');
+    expect(sutta_json.translation_text['an1.1:1.1']).toBe('So I have heard. ');
+});
